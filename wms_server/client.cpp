63d57795e8590120a097aa36247b298defc448b8
anotherLostKitten <eggismagicchickensquared@gmail.com>
2024-06-25 05:54:44 -0400

    threaded connection handlers

--- wms_server/client.cpp
+++ wms_server/client.cpp
@@ -22,5 +22,21 @@ int main() {
     if (!conn.read_timeout(TIMEOUT))
         exit(1);
 
+    string msg = "test";
+    char buf[512];
+
+    while (1) {
+        sockpp::result<size_t> res;
+
+        if (conn.write(msg) != msg.length())
+            exit(1);
+
+        res = conn.read(buf, sizeof(buf));
+        if (!res || res.value() <= 0)
+            exit(1);
+
+        cout << "recieved: " << buf << endl;
+    }
+
     return 0;
 }
--- wms_server/server.cpp
+++ wms_server/server.cpp
@@ -1,9 +1,23 @@
 #include <iostream>
 #include <string>
+#include <thread>
 
 #include "sockpp/tcp_acceptor.h"
 
 using namespace std;
+
+void handler(sockpp::tcp_socket sock) {
+    char buf[512];
+    sockpp::result<size_t> res;
+
+    while (1) {
+        res = sock.read(buf, sizeof(buf));
+        if (!res || res.value() <= 0)
+            break;
+        cout << "recieved: " << buf << endl;
+        sock.write_n(buf, res.value());
+    }
+}
 
 int main() {
     in_port_t port = sockpp::TEST_PORT;
@@ -25,6 +39,9 @@ int main() {
         if (res) {
             cout << "connection with " << peer << endl;
             sockpp::tcp_socket sock = res.release();
+
+            thread thr(handler, std::move(sock));
+            thr.detach();
         } else
             cout << "error " << res.error_message();
     }
