51bc168b4abc22a680b81c5da7fe180b6220bac7
anotherLostKitten <eggismagicchickensquared@gmail.com>
2024-06-26 09:40:55 -0400

    bbox partitioning

--- wms_server/wms.cpp
+++ wms_server/wms.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
 #include <format>
+#include <cmath>
+#include <memory>
+#include <stdint.h>
 
 #include "wms.h"
+
+#define BBOX_PER_DEG ((float)100)
 
 using namespace std;
 
@@ -14,4 +19,26 @@ void print_bbox(const struct bbox* query) {
 
 string get_bbox_filename(const struct bbox* query) {
     return format("map_bbox_{}_{}_{}_{}.geojson", query->minx, query->miny, query->maxx, query->maxy);
+}
+
+size_t create_normalized_bbox(const struct bbox* outer, unique_ptr<struct bbox[]>* arr) {
+    float minx = floor(outer->minx * BBOX_PER_DEG) / BBOX_PER_DEG;
+    float miny = floor(outer->miny * BBOX_PER_DEG) / BBOX_PER_DEG;
+    float maxx = floor(outer->maxx * BBOX_PER_DEG) / BBOX_PER_DEG;
+    float maxy = floor(outer->maxy * BBOX_PER_DEG) / BBOX_PER_DEG;
+
+    size_t bboxes_x = (size_t)round(abs(maxx-minx) * BBOX_PER_DEG),
+        bboxes_y = (size_t)round(abs(maxy-miny) * BBOX_PER_DEG);
+
+    *arr = make_unique<struct bbox[]>(bboxes_x * bboxes_y);
+
+    for (size_t x = 0; x < bboxes_x; x++)
+        for (size_t y = 0; y < bboxes_y; y++)
+            (*arr)[x * bboxes_y + y] = (struct bbox){
+                .minx = minx + (float)x / BBOX_PER_DEG,
+                .miny = miny + (float)y / BBOX_PER_DEG,
+                .maxx = minx + (float)(x+1) / BBOX_PER_DEG,
+                .maxy = miny + (float)(y+1) / BBOX_PER_DEG,
+            };
+    return bboxes_x * bboxes_y;
 }
--- wms_server/wms.h
+++ wms_server/wms.h
@@ -13,3 +13,5 @@ struct bbox {
 void print_bbox(const struct bbox* query);
 
 std::string get_bbox_filename(const struct bbox* query);
+
+size_t create_normalized_bbox(const struct bbox* outer, std::unique_ptr<struct bbox[]>* arr);
