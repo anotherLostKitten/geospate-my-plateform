b4ac6d2a5f45d48ab07240246e77835f98277a30
anotherLostKitten <eggismagicchickensquared@gmail.com>
2024-06-27 09:34:03 -0400

    existing file check )

--- wms_server/chunk_manager.cpp
+++ wms_server/chunk_manager.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <filesystem>
+#include <vector>
 
 #include "wms.h"
 #include "osm_api.h"
@@ -8,13 +9,15 @@
 
 using namespace std;
 
-int check_bbox_local_file(const struct bbox* bbox, unique_ptr<string>* file_names) {
-    int n = 0;
+vector<string> check_bbox_local_file(const struct bbox* bbox) {
+    vector<string> fs;
+    string fn = get_bbox_filename(bbox);
     for (const filesystem::directory_entry entry
-             : filesystem::directory_iterator(GEOJSON_PATH)) {
-        cout << entry << endl;
-    }
-    return n;
+             : filesystem::directory_iterator(GEOJSON_PATH))
+        if (entry.is_regular_file() && fn.find(entry.path().stem()) != string::npos)
+            fs.push_back(entry.path().filename());
+
+    return fs;
 }
 
 void load_bbox(const struct bbox* outer_bbox) {
